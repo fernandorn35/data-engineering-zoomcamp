3a8ce8a0b77cf0aeabb0ee06a6c1b9a67c8e2655
Fernando Rosales <90079558+fernandorn35@users.noreply.github.com>
2023-04-08 12:38:32 -0400

    Update test.php

--- test.php
+++ test.php
@@ -1,1 +1,6 @@
 
+Connect Git Bash to GitHub with SSH
+https://www.youtube.com/watch?v=iWs34DO_H2M
+
+Ayuda!! Cuando hago el comando: “git pull origin master” me sale: “fatal: couldn´t find remote ref master
+https://platzi.com/discusiones/1557-git-github/66661-ayuda-cuando-hago-el-comando-git-pull-origin-master-me-sale-fatal-couldn-t-find-remote-ref-master/
